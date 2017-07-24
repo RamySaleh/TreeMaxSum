cce79a8704e4524e50e820b367f599accd948427
RamySaleh <ramy.saleh.me@gmail.com>
2017-07-24 22:41:42 +0200

    cleaning

---
+++ Emara.CodingTest/Helpers/IntExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Emara.CodingTest.Helpers
+{
+    public static class IntExtensions
+    {
+        public static bool IsEven(this int value)
+        {
+            return value % 2 == 0;
+        }
+    }
+}
--- Emara.CodingTest/TreeMaxSumAlgorithm.cs
+++ Emara.CodingTest/TreeMaxSumAlgorithm.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emara.CodingTest.Helpers;
 
 namespace Emara.CodingTest
 {
@@ -14,31 +15,31 @@ namespace Emara.CodingTest
             var max = 0;
             if (data.Length > 0 && data[0].Length > 0)
             {
-                max = CalculateTreeMax(data, 0, 0, 0, IsEven(data[0][0]), 0);
+                max = CalculateTreeMax(data, 0, 0, 0, data[0][0].IsEven(), 0);
             }
 
             return max;
         }
 
-        public static int CalculateTreeMax(int[][] data, int i, int j, int currentMax, bool isPrevEven, int max)
+        private static int CalculateTreeMax(int[][] data, int i, int j, int currentMax, bool isPrevEven, int max)
         {
             currentMax += data[i][j];
 
             if (i + 1 < data.Length)
             {
                 var leftNode = data[i + 1][j];
-                if (IsEven(leftNode) != isPrevEven)
+                if (leftNode.IsEven() != isPrevEven)
                 {
-                    max = CalculateTreeMax(data, i + 1, j, currentMax, IsEven(leftNode), max);
+                    max = CalculateTreeMax(data, i + 1, j, currentMax, leftNode.IsEven(), max);
                 }
             }
             
             if (i + 1 < data.Length && j + 1 < data[i+1].Length)
             {
                 var rightNode = data[i + 1][j + 1];
-                if (IsEven(rightNode) != isPrevEven)
+                if (rightNode.IsEven() != isPrevEven)
                 {
-                    max = CalculateTreeMax(data, i + 1, j + 1, currentMax, IsEven(rightNode), max);
+                    max = CalculateTreeMax(data, i + 1, j + 1, currentMax, rightNode.IsEven(), max);
                 }
             }
 
@@ -48,11 +49,6 @@ namespace Emara.CodingTest
             }
 
             return max;
-        }
-
-        private static bool IsEven(int value)
-        {
-            return value % 2 == 0;
         }
     }
 }
